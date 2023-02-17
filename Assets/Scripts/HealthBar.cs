414913fc815fecd28191045a1aa7ea862eaacd1c
PC-imatge-2022-2023 <licenses@enti.cat>
2023-02-17 10:39:19 +0100

    no message

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,12 +5,9 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Image healthBar;
-    public Image healthBar1;
-    public Image healthBar2;
-    public Image healthBar3;
-    public Image healthBar4;
-    public Image ImgMedio;
+    public Image healthHeart;
+    public Image healthHeart1;
+    public Image healthHeart2;
 
     void Start()
     {
@@ -21,77 +18,10 @@ public class HealthBar : MonoBehaviour
    
     void Update()
     {
-
-        IEnumerator ImageMedio()
-        {
-            healthBar.enabled = false;
-            healthBar1.enabled = true;
-            yield return new WaitForSeconds((float)0.2);
-            ImgMedio.enabled = true;
-            yield return new WaitForSeconds((float)0.2);
-            ImgMedio.enabled = false;
-            healthBar2.enabled = false;
-            healthBar3.enabled = false;
-            healthBar4.enabled = false;
-
-        }
-            if (GameManager.instance.life == 100)
-        {
-            healthBar.enabled = true;
-            healthBar1.enabled = false;
-            healthBar2.enabled = false;
-            healthBar3.enabled = false;
-            healthBar4.enabled = false;
-
-
-        }
-        if (GameManager.instance.life == 80)
-        {
-            StartCoroutine(ImageMedio());
-
-        }
-        if (GameManager.instance.life == 60)
-        {
-            healthBar.enabled = false;
-            healthBar1.enabled = false;
-            healthBar2.enabled = true;
-            healthBar3.enabled = false;
-            healthBar4.enabled = false;
-
-        }
-        if (GameManager.instance.life == 40)
-        {
-            healthBar.enabled = false;
-            healthBar1.enabled = false;
-            healthBar2.enabled = false;
-            healthBar3.enabled = true;
-            healthBar4.enabled = false;
-
-        }
-        if (GameManager.instance.life == 20)
-        {
-            healthBar.enabled = false;
-            healthBar1.enabled = false;
-            healthBar2.enabled = false;
-            healthBar3.enabled = false;
-            healthBar4.enabled = true;
-
-        }
-        if (GameManager.instance.life == 0)
-        {
-            healthBar.enabled = false;
-            healthBar1.enabled = false;
-            healthBar2.enabled = false;
-            healthBar3.enabled = false;
-            healthBar4.enabled = false;
-
-        }
-
-
-
-
-
-
+        
+        healthHeart.enabled = false;
+        healthHeart1.enabled = false;
+        healthHeart2.enabled = false;
     }
 
 
